aebfb733637c41e7a90f1b12baf2c50803b21a8f
Maxima24 <154669600+Maxima24@users.noreply.github.com>
2025-08-08 00:01:16 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server:{
-    host:"0.0.0.0"
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0"
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'react-vendor'
+            if (id.includes('tailwindcss')) return 'tailwind-vendor'
+            return 'vendor'
+          }
+        }
+      }
+    }
   }
 })
-
